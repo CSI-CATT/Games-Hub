7bfce93547fb9e1e341c2dfaf16dc2341393a286
Diya Mondal <diyam161819@gmail.com>
2025-10-15 00:36:39 +0530

    Updated Landing Page UI

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,13 +4,11 @@ import { Link, useLocation } from 'react-router-dom'
 export default function Nav() {
   const location = useLocation()
   return (
-    <nav className="nav">
-      <Link to="/" className="logo">🎮 Fun Games Hub</Link>
-      <div className="nav-links">
-        <Link to="/tictactoe" className={location.pathname === "/tictactoe" ? "active" : ""}>Tic Tac Toe</Link>
-        <Link to="/memory" className={location.pathname === "/memory" ? "active" : ""}>Memory</Link>
-        <Link to="/snake" className={location.pathname === "/snake" ? "active" : ""}>Snake</Link>
-        <Link to="/RockPaperScissors" className={location.pathname === "/RockPaperScissors" ? "active" : ""}>RockPaperScissors</Link>
+    <nav className="w-full sticky top-0 z-50 h-16 flex justify-center md:justify-between items-center bg-gray-800 px-12 border-b-gray-50">
+      <Link to="/" className="text-white text-xl flex-shrink-0 font-extrabold tracking-tight">🎮 Fun Games Hub</Link>
+      <div className="hidden md:flex space-x-8 font-semibold">
+          <a href="#games">Games</a>
+          <a href="https://github.com/CSI-CATT/Games-Hub">Contributions</a>
       </div>
     </nav>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,42 @@
 import React from 'react'
 import GameCard from '../components/GameCard'
+import TicTacToeIcon from '../assets/tic-tac-toe.svg';
+import MemoryMatchIcon from '../assets/card-pickup.svg';
+import SnakeIcon from '../assets/snake.svg';
+import TypingIcon from '../assets/keyboard.svg';
+import WordScrambleIcon from '../assets/notebook.svg';
+import RPSIcon from '../assets/scissors.svg';
+// svg icons taken from the website https://game-icons.net/
 
 export default function Home() {
   return (
-    <div>
-      <header className="hero">
-        <h1>Welcome to Fun Games Hub 🎉</h1>
-        <p>A hub of fun games made for Hacktoberfest contributions!</p>
+    <div className='max-w-8xl mx-auto h-screen flex flex-col gap-16'>
+      <header className="max-w-full min-h-[80vh] bg-gradient-to-br from-pink-500/70 to-blue-500/70 flex flex-col justify-center items-center gap-8">
+        <h1 className='text-4xl text-center sm:text-7xl font-extrabold '>Welcome to Fun Games Hub 🎉</h1>
+        <p className='text-xl sm:text-2xl text-center'>A hub of fun games made for Hacktoberfest contributions!</p>
       </header>
 
-      <section className="games-grid">
-        <GameCard title="Tic Tac Toe" desc="Classic 3x3 game" to="/tictactoe" />
-        <GameCard title="Memory Match" desc="Flip and match pairs" to="/memory" />
-        <GameCard title="Snake" desc="Classic snake game" to="/snake" />
-        <GameCard title="Typing Speed Game" desc= "Play and test your typing speed" to="/typing-speed" />
-        
-        {/* --- ADD THIS LINE FOR YOUR NEW GAME --- */}
+      <section id="games" className="games">
+        <h3 className='text-3xl text-center font-semibold'>Explore Featured Games</h3>
+        <div className='max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-8 '>
+        <GameCard title="Tic Tac Toe" desc="Classic 3x3 game" to="/tictactoe" className="aspect-square" imageUrl={TicTacToeIcon}/>
+        <GameCard title="Memory Match" desc="Flip and match pairs" to="/memory" className="aspect-square" imageUrl={MemoryMatchIcon}/>
+        <GameCard title="Snake" desc="Classic snake game" to="/snake" className="aspect-square" imageUrl={SnakeIcon} />
+        <GameCard title="Typing Speed Game" desc= "Play and test your typing speed" to="/typing-speed" className="aspect-square" imageUrl={TypingIcon} />
         <GameCard 
           title="Word Scramble" 
           desc="Unscramble letters to find the hidden word" 
           to="/word-scramble" 
+          className="aspect-square"
+          imageUrl={WordScrambleIcon}
         />
-        
-        <GameCard title="Rock Paper Scissors" desc="Classic Rock Paper Scissors game" to="/RockPaperScissors" />
+        <GameCard title="Rock Paper Scissors" desc="Classic Rock Paper Scissors game" to="/RockPaperScissors" className="aspect-square"imageUrl={RPSIcon}/>
+        </div>
       </section>
+
+      <footer className='text-center bg-gray-800'>
+        CSI-CATT DMCE &copy; 2025
+      </footer>
     </div>
   )
 }
